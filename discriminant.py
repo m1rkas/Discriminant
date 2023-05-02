4dcde94e933541eada996d18c681a271f3e3579e
m1rkas <martem2108@gmail.com>
2023-05-02 17:13:14 +0300

    Find a discriminant and x

---
+++ discriminant.py
@@ -0,0 +1,23 @@
+import math
+a = int(input("a: "))
+b = int(input("b: "))
+c = int(input("c: "))
+
+D = b**2 -4*a*c
+print("D =", D)
+
+if D < 0:
+    print("x є ∅")
+elif D == 0:
+    x = -b / (2*a)
+    print("x =", x)
+else:
+    if math.sqrt(D) == int(math.sqrt(D)):
+        x1 = (-b + math.sqrt(D)) / (2*a)
+        x2 = (-b - math.sqrt(D)) / (2*a)
+        print("x1 =", x1)
+        print("x2 =", x2)
+    else:
+        print("x1 =", str(-b) + "+√" + str(D), "/", 2*a)
+        print("x2 =", str(-b) + "-√" + str(D), "/", 2*a)
+
