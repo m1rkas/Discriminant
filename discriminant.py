524f1a5b410060e293d100b21bddd496a0926f75
m1rkas <martem2108@gmail.com>
2023-05-02 22:49:50 +0300

    Describe x in detail

--- discriminant.py
+++ discriminant.py
@@ -14,12 +14,12 @@ while restart.lower() == "yes" or restart.lower() == "y" or restart == "":
         print("x є ∅")
     elif D == 0:
         x = -b / (2*a)
-        print("x =", x)
+        print("x =", -b, "/ (2*" + str(a) + ") =", -b, "/", 2*a, "=", x)
     elif math.sqrt(D) == int(math.sqrt(D)):
-            x1 = (-b + math.sqrt(D)) / (2*a)
-            x2 = (-b - math.sqrt(D)) / (2*a)
-            print("x1 =", x1)
-            print("x2 =", x2)
+        x1 = (-b + math.sqrt(D)) / (2*a)
+        x2 = (-b - math.sqrt(D)) / (2*a)
+        print("x1 = (" + str(-b), "+ √" + str(D) + ") / (2*" + str(a) + ") = (" + str(-b), "+", str(int(math.sqrt(D))) + ") /", 2*a, "=" , -b + math.sqrt(D), "/", 2*a, "=", x1)
+        print("x2 = (" + str(-b), "- √" + str(D) + ") / (2*" + str(a) + ") = (" + str(-b), "-", str(int(math.sqrt(D))) + ") /", 2*a, "=" , -b - math.sqrt(D), "/", 2*a, "=", x2)
     else:
         print("x1 =", str(-b) + "+√" + str(D), "/", 2*a)
         print("x2 =", str(-b) + "-√" + str(D), "/", 2*a)
